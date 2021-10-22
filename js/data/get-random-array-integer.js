a3cf8296279a6591a80f2e2ea394f93d9925c4e0
Malina <i.malinovskaya@big3.ru>
2021-10-22 10:34:05 +0300

    модуляция , добавила default

--- js/data/get-random-array-integer.js
+++ js/data/get-random-array-integer.js
@@ -2,5 +2,5 @@ import {getRandomInteger} from '../utils/get-random-integer.js';
 
 const getRandomArrayInteger = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-export {getRandomArrayInteger};
+export default getRandomArrayInteger;
 
